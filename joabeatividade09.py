10c4c6d25a5d86d30f3c0d82e8f17a2f75f8bb9f
Joabe Jordão de Souza Junior <146821705+Joadejdsj@users.noreply.github.com>
2023-10-18 11:49:38 -0300

    Add files via upload

---
+++ joabeatividade09.py
@@ -0,0 +1,7 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+n1 = int(input("digite um número: "))
+n2 = int(input("digite um número: "))
+if  n1 >= n2 : 
+    print("O número maior é: " , n1)
+else: 
+    print("O número maior é: " , n2)
